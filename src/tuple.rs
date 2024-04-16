08023b799fa4a0217d8047c6468997cc4599289b
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-04-15 22:00:48 -0500

    add +, minus, neg to tuple

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,10 +1,14 @@
-#[derive(Default, Debug, Copy, Clone)]
+use std::ops::{Add, Sub, Neg};
+
+#[derive(Default, Debug, Copy, Clone, PartialEq)]
 pub struct Tuple {
     pub x: f64,
     pub y: f64,
     pub z: f64,
     pub w: f64,
 }
+
+const EPSILON: f64 = 0.00001;
 
 pub type Vector = Tuple;
 pub type Point = Tuple;
@@ -15,4 +19,43 @@ pub const fn vector(x: f64, y: f64, z: f64) -> Point {
 
 pub const fn point(x: f64, y: f64, z: f64) -> Vector {
     Vector { x, y, z, w: 1.0 }
+}
+
+impl Add for Tuple {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl Sub for Tuple {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl Neg for Tuple {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x, 
+            y: -self.y,
+            z: -self.z,
+            w: -self.w,
+        }
+    }
 }
--- tests/tuples.rs
+++ tests/tuples.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, ops::Neg};
+
 use cucumber::{given, then, World};
 
 // Need to explicitly add path to mod.
@@ -12,57 +14,60 @@ use tuple::*;
 #[derive(Debug, Default, World)]
 pub struct TupleWorld {
     tuple: Tuple,
+    point: Point,
+    vector: Vector,
+    a1: Tuple,
+    a2: Tuple,
+    tuples: HashMap<String, Tuple>,
 }
 
 /* Givens */
-
-#[given(expr = "a <- tuple[{float}, {float}, {float}, {float}]")]
-fn create_tuple(world: &mut TupleWorld, x: f64, y: f64, z: f64, w: f64) {
-    world.tuple = Tuple {
-        x: x,
-        y: y,
-        z: z,
-        w: w,
-    };
+// TODO make these more robust
+#[given(expr = "{word} <- tuple[{float}, {float}, {float}, {float}]")]
+fn create_tuple(world: &mut TupleWorld, key: String, x: f64, y: f64, z: f64, w: f64) {
+    world.tuples.insert(key, Tuple { x, y, z, w });
+    ()
 }
 
-#[given(expr = "p <- point[{float}, {float}, {float}]")]
-fn create_point(world: &mut TupleWorld, x: f64, y: f64, z: f64) {
-    world.tuple = point(x, y, z);
+#[given(expr = "{word} <- point[{float}, {float}, {float}]")]
+fn create_point(world: &mut TupleWorld, key: String, x: f64, y: f64, z: f64) {
+    world.tuples.insert(key, point(x, y, z));
+    ()
 }
 
-#[given(expr = "p <- vector[{float}, {float}, {float}]")]
-fn create_vector(world: &mut TupleWorld, x: f64, y: f64, z: f64) {
-    world.tuple = vector(x, y, z);
+#[given(expr = "{word} <- vector[{float}, {float}, {float}]")]
+fn create_vector(world: &mut TupleWorld, key: String, x: f64, y: f64, z: f64) {
+    world.tuples.insert(key, vector(x, y, z));
+    ()
 }
 
 /* Thens */
 
-#[then(expr = "a.x = {float}")]
-fn test_x(world: &mut TupleWorld, x: f64) {
-    assert!(world.tuple.x == 4.3);
+#[then(expr = "{word}.x = {float}")]
+fn test_x(world: &mut TupleWorld, key: String, x: f64) {
+    assert!(world.tuples.get(&key).unwrap().x == x);
 }
 
-#[then(expr = "a.y = {float}")]
-fn test_y(world: &mut TupleWorld, y: f64) {
-    assert!(world.tuple.y == y);
+#[then(expr = "{word}.y = {float}")]
+fn test_y(world: &mut TupleWorld, key: String, y: f64) {
+    assert!(world.tuples.get(&key).unwrap().y == y);
 }
 
-#[then(expr = "a.z = {float}")]
-fn test_z(world: &mut TupleWorld, z: f64) {
-    assert!(world.tuple.z == z);
+#[then(expr = "{word}.z = {float}")]
+fn test_z(world: &mut TupleWorld, key: String, z: f64) {
+    assert!(world.tuples.get(&key).unwrap().z == z);
 }
 
-#[then(expr = "a.w = {float}")]
-fn test_w(world: &mut TupleWorld, w: f64) {
-    assert!(world.tuple.w == w);
+#[then(expr = "{word}.w = {float}")]
+fn test_w(world: &mut TupleWorld, key: String, w: f64) {
+    assert!(world.tuples.get(&key).unwrap().w == w);
 }
 
-#[then(regex = "^a is a (vector|point)$")]
-fn is_a_point(world: &mut TupleWorld, tuple: String) {
+#[then(expr = "{word} is a {word}")]
+fn is_a_point(world: &mut TupleWorld, key: String, tuple: String) {
     match tuple.as_str() {
-        "point" => assert!(world.tuple.w == 1.0),
-        "vector" => assert!(world.tuple.w == 0.0),
+        "point" => assert!(world.tuples.get(&key).unwrap().w == 1.0),
+        "vector" => assert!(world.tuples.get(&key).unwrap().w == 0.0),
         _ => unreachable!(),
     }
 }
@@ -72,22 +77,69 @@ fn is_a_point(world: &mut TupleWorld, tuple: String) {
 * using []'s for method construction
 */
 
-#[then(regex = "^a is not a (vector|point)$")]
-fn is_not_a_vector(world: &mut TupleWorld, tuple: String) {
+#[then(expr = "{word} is not a {word}")]
+fn is_not_a_vector(world: &mut TupleWorld, key: String, tuple: String) {
     match tuple.as_str() {
-        "point" => assert!(world.tuple.w != 1.0),
-        "vector" => assert!(world.tuple.w != 0.0),
+        "point" => assert!(world.tuples.get(&key).unwrap().w != 1.0),
+        "vector" => assert!(world.tuples.get(&key).unwrap().w != 0.0),
         _ => unreachable!(),
     }
 }
 
-#[then(expr = "p = tuple[{float}, {float}, {float}, {float}]")]
-fn p_equals(world: &mut TupleWorld, x: f64, y: f64, z: f64, w: f64) {
-    assert!(world.tuple.x == x);
-    assert!(world.tuple.y == y);
-    assert!(world.tuple.z == z);
-    assert!(world.tuple.w == w);
+#[then(expr = "{word} = tuple[{float}, {float}, {float}, {float}]")]
+fn p_equals(world: &mut TupleWorld, key: String, x: f64, y: f64, z: f64, w: f64) {
+    let mut n: Tuple = *world.tuples.get(&key).unwrap();
+    assert!(n.x == x);
+    assert!(n.y == y);
+    assert!(n.z == z);
+    assert!(n.w == w);
 }
+#[then(expr = "- {word} = tuple[{float}, {float}, {float}, {float}]")]
+fn neg_equals(world: &mut TupleWorld, key: String, x: f64, y: f64, z: f64, w: f64) {
+    let mut n: Tuple = -*world.tuples.get(&key).unwrap();
+    assert!(n.x == x);
+    assert!(n.y == y);
+    assert!(n.z == z);
+    assert!(n.w == w);
+}
+
+#[then(expr = "{word} + {word} = tuple[{float}, {float}, {float}, {float}]")]
+fn tuple_add_equals(
+    world: &mut TupleWorld,
+    key1: String,
+    key2: String,
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
+) {
+    let n: Tuple =
+        *world.tuples.get(&key1).unwrap() + *world.tuples.get_mut(&key2).unwrap();
+    assert!(n.x == x);
+    assert!(n.y == y);
+    assert!(n.z == z);
+    assert!(n.w == w);
+}
+
+#[then(expr = "{word} - {word} = {word}[{float}, {float}, {float}]")]
+fn point_minus_equals(
+    world: &mut TupleWorld,
+    key1: String,
+    key2: String,
+    f_or_v: String,
+    x: f64,
+    y: f64,
+    z: f64
+) {
+    let n: Tuple =
+        *world.tuples.get(&key1).unwrap() - *world.tuples.get_mut(&key2).unwrap();
+    assert!(n.x == x);
+    assert!(n.y == y);
+    assert!(n.z == z);
+    let w = if f_or_v == "vector" {0.0} else {1.0};
+    assert!(n.w == w);
+}
+
 
 // This runs before everything else, so you can setup things here.
 fn main() {
